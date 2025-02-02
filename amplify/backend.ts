ab664eca5838c50bf511f907286370e1de317451
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-02-01 23:28:32 -0800

    Use manually customized VPC

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,30 +4,21 @@ import { defineBackend } from '@aws-amplify/backend'
 
 import { Duration } from 'aws-cdk-lib'
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway'
-import { NatProvider, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2'
+import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2'
 import { Runtime } from 'aws-cdk-lib/aws-lambda'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 
 import 'dotenv/config'
-const eipAllocationIds = process.env.EIP_ALLOCATION_IDS!.split(',')
 
 const backend = defineBackend({})
 
 const vpcStack = backend.createStack('vpc')
 
-const vpc = new Vpc(vpcStack, 'LambdaVpc', {
-  natGatewayProvider: NatProvider.gateway({ eipAllocationIds }),
-  subnetConfiguration: [
-    { name: 'Isolated', subnetType: SubnetType.PRIVATE_ISOLATED },
-    { name: 'Private', subnetType: SubnetType.PRIVATE_WITH_EGRESS },
-    { name: 'Public', subnetType: SubnetType.PUBLIC },
-  ],
+const vpc = Vpc.fromVpcAttributes(vpcStack, 'ImportVPC', {
+  vpcId: process.env.VPC_ID!,
+  availabilityZones: process.env.AVAILABILITY_ZONES!.split(','),
+  privateSubnetIds: process.env.PRIVATE_SUBNET_IDS!.split(','),
 })
-const lambdaSecurityGroup = new SecurityGroup(
-  vpcStack,
-  'Lambda Security Group',
-  { vpc: vpc }
-)
 
 const lambda = new NodejsFunction(vpcStack, 'vpcLambda', {
   entry: path.join(__dirname, 'functions', 'get-ip', 'handler.ts'),
@@ -35,7 +26,6 @@ const lambda = new NodejsFunction(vpcStack, 'vpcLambda', {
   timeout: Duration.seconds(30),
   vpc: vpc,
   vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
-  securityGroups: [lambdaSecurityGroup],
 })
 
 const api = new LambdaRestApi(vpcStack, 'restApiForVpcLambda', {
